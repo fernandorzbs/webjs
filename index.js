73214f0c248fc69c29ced65665390a9823cae522
Fernando Rz <fernandorz.popup@gmail.com>
2024-06-04 16:15:21 -0400

    webjs

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    }
+});
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
